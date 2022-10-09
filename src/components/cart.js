b3d9a5c5df688e57820eb2755ca03d1563e0034e
Your Name <you@example.com>
2022-10-09 15:14:16 +0530

    09/10/2022

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,7 +1,62 @@
 import React from 'react'
+import { Row, Col, Typography } from 'antd'
+import '../Style/cart.less'
+
+const { Title } = Typography;
 
 export default function cart() {
+  
   return (
-    <div>cart</div>
+    <>
+      <div className="main-cart">
+          <Title level={1} style={{color:'#251B37', margin:'15px 20px'}}>Cart</Title>
+        <div className="cartList">
+          <Row gutter={[18, 28]}>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+              <Col className='gutter-row' span={8}>
+                <div className='cartItem'>col-8</div>
+              </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+            <Col className='gutter-row' span={8}>
+              <div className='cartItem'>col-8</div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </>
   )
 }
